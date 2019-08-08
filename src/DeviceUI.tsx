89403b6d5eb918b8827736ad7687dc9880d7ec2d
Markus Tacker <Markus.Tacker@NordicSemi.no>
2019-08-08 17:12:04 +0200

    feat: display config

--- src/DeviceUI.tsx
+++ src/DeviceUI.tsx
@@ -13,20 +13,33 @@ const Device = ({
 	children,
 }: {
 	endpoint: string
-	children: (args: { deviceId: string }) => JSX.Element | null
+	children: (args: { deviceId: string; config: any }) => JSX.Element | null
 }): JSX.Element | null => {
 	const [deviceId, setDeviceId] = useState('')
+	const [config, setConfig] = useState()
 	useEffect(() => {
 		fetch(`${endpoint}/id`)
 			.then(async response => {
 				setDeviceId(await response.text())
 			})
 			.catch(err => console.error(err))
+
+		const connection = new WebSocket(endpoint.replace(/^https?/, 'ws'))
+		connection.onopen = () => {
+			console.debug('[ws]', 'open')
+		}
+		connection.onerror = error => {
+			console.error('[ws]', error)
+		}
+		connection.onmessage = message => {
+			console.debug('[ws]', 'message', message)
+			setConfig(JSON.parse(message.data))
+		}
 	}, [endpoint])
 	if (!deviceId) {
 		return <p>Connecting to {endpoint} ...</p>
 	}
-	return children({ deviceId })
+	return children({ deviceId, config })
 }
 
 export const DeviceUIApp = ({ endpoint }: { endpoint: string }) => {
@@ -59,14 +72,14 @@ export const DeviceUIApp = ({ endpoint }: { endpoint: string }) => {
 			</header>
 			<main>
 				<Device endpoint={endpoint}>
-					{({ deviceId }) => (
+					{({ deviceId, config }) => (
 						<form>
-							{error && (
-								<Alert color="danger">{JSON.stringify(error)}</Alert>
-							)}
+							{error && <Alert color="danger">{JSON.stringify(error)}</Alert>}
 							<dl>
 								<dt>DeviceId</dt>
 								<dd>{deviceId}</dd>
+								<dt>Config</dt>
+								<dd>{JSON.stringify(config)}</dd>
 								<dt>Battery voltage: {batteryVoltage}</dt>
 								<dd>
 									<Slider
