fa84f6fc22dd66db405ae2a77fa3e3431b7549c9
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-01-13 14:47:14 +0100

    fix: update config for Snowpack v3

--- snowpack.config.js
+++ snowpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 		src: '/_dist_',
 	},
 	plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-typescript'],
-	installOptions: {
+	packageOptions: {
 		installTypes: true,
 		env: {
 			SNOWPACK_PUBLIC_VERSION: true,
