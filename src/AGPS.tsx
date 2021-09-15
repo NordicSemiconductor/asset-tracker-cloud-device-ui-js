16b991dca60b09627585137db5bfc1e0fac470aa
Markus Tacker <m@coderbyheart.com>
2021-09-15 11:12:47 +0200

    feat: add UI to update roaming info

--- src/AGPS.tsx
+++ src/AGPS.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment, useContext, useState } from 'react'
 import styled from 'styled-components'
 import { MessageContext } from './Device'
-import * as MccMncList from 'mcc-mnc-list'
 
 const Headline = styled.h2`
 	font-size: 100%;
@@ -54,13 +53,17 @@ const AGPSDataTypes = {
 
 export const AGPS = ({
 	sendMessage: m,
+	mcc,
+	mnc,
+	cell,
+	area,
 }: {
 	sendMessage: (message: Record<string, any>, topic: string) => void
+	mcc: number
+	mnc: number
+	cell: number
+	area: number
 }) => {
-	const [mcc, setMcc] = useState<string>('242')
-	const [mnc, setMnc] = useState<string>('2')
-	const [cell, setCell] = useState<number>(33703719)
-	const [area, setArea] = useState<number>(12)
 	const [types, setTypes] = useState<number[]>([1, 2, 3, 4, 6, 7, 8, 9])
 
 	const { messages } = useContext(MessageContext)
@@ -70,79 +73,6 @@ export const AGPS = ({
 		<>
 			<form>
 				<Headline>Assisted GPS</Headline>
-				<Fieldset>
-					<label htmlFor="mcc">Mobile Country Code:</label>
-					<select
-						id="mcc"
-						value={mcc}
-						onChange={({ target: { value } }) => setMcc(value)}
-					>
-						{Object.entries(
-							MccMncList.all()
-								.filter(({ status }) => status === 'Operational')
-								.sort(({ countryName: c1 }, { countryName: c2 }) =>
-									(c1 ?? 'Z').localeCompare(c2 ?? 'Z'),
-								)
-								.reduce(
-									(mccs, { mcc, countryName }) => ({
-										...mccs,
-										[mcc]: countryName,
-									}),
-									{} as Record<string, string>,
-								),
-						).map(([mcc, countryName]) => (
-							<option key={mcc} value={mcc}>
-								{countryName ?? mcc} ({mcc})
-							</option>
-						))}
-					</select>
-				</Fieldset>
-				<Fieldset>
-					<label htmlFor="mnc">Mobile Network Code:</label>
-					{mcc === undefined && (
-						<select id="mnc" disabled>
-							<option>Please select a MCC</option>
-						</select>
-					)}
-					{mcc !== undefined && (
-						<select
-							id="mnc"
-							value={mnc}
-							onChange={({ target: { value } }) => setMnc(value)}
-						>
-							{MccMncList.all()
-								.filter(({ mcc: m }) => m === mcc)
-								.sort(({ mnc: m1 }, { mnc: m2 }) => m1.localeCompare(m2))
-								.map(({ mnc, brand, operator }) => (
-									<option key={mnc} value={mnc}>
-										{brand} {operator} ({mnc})
-									</option>
-								))}
-						</select>
-					)}
-				</Fieldset>
-				<Fieldset>
-					<label htmlFor="cell">Cell:</label>
-					<input
-						id="cell"
-						type="number"
-						step={1}
-						min={0}
-						value={cell}
-						onChange={({ target: { value } }) => setCell(parseInt(value, 10))}
-					/>
-				</Fieldset>
-				<Fieldset>
-					<label htmlFor="area">Area:</label>
-					<input
-						id="area"
-						type="number"
-						step={1}
-						min={0}
-						value={area}
-						onChange={({ target: { value } }) => setArea(parseInt(value, 10))}
-					/>
-				</Fieldset>
 				<FieldsetWithCheckboxes>
 					{Object.entries(AGPSDataTypes).map(([k, v]) => {
 						const t = parseInt(k, 10)
@@ -170,10 +100,10 @@ export const AGPS = ({
 						onClick={() => {
 							m(
 								{
-									mcc: parseInt(mcc ?? '242', 10),
-									mnc: parseInt(mnc ?? '2', 10),
-									cell: cell ?? 33703719,
-									area: area ?? 12,
+									mcc,
+									mnc,
+									cell,
+									area,
 									types: types ?? [1, 2, 3, 4, 6, 7, 8, 9],
 								},
 								'agps/get',
--- src/UpdateUI.tsx
+++ src/UpdateUI.tsx
@@ -7,6 +7,7 @@ import { AccelerometerSlider } from './AccelerometerSlider'
 import { Device } from './Device'
 import { PGPS } from './PGPS'
 import { AGPS } from './AGPS'
+import * as MccMncList from 'mcc-mnc-list'
 
 const DeviceInfoList = styled.dl`
 	dd + dt {
@@ -38,6 +39,11 @@ export const UpdateUI = ({
 	const [temp, setTemp] = useState(21)
 	const [hum, setHum] = useState(50)
 	const [rsrp, setRSRP] = useState(70)
+	const [nw, setNw] = useState('LAN')
+	const [mcc, setMcc] = useState<string>('242')
+	const [mnc, setMnc] = useState<string>('2')
+	const [cell, setCell] = useState<number>(33703719)
+	const [area, setArea] = useState<number>(12)
 
 	return (
 		<Device endpoint={endpoint}>
@@ -49,6 +55,141 @@ export const UpdateUI = ({
 							<dd>{deviceId}</dd>
 							<dt>Config</dt>
 							<dd>{JSON.stringify(config)}</dd>
+							<dt>Network</dt>
+							<dd>
+								<select
+									onChange={({ target: { value } }) => {
+										setNw(value)
+										u({
+											property: 'dev',
+											v: {
+												nw: value,
+											},
+										})
+									}}
+									value={nw}
+								>
+									<option>LAN</option>
+									<option>WiFi</option>
+									<option>LTE-M GPS</option>
+									<option>LTE-M</option>
+									<option>NB-IoT GPS</option>
+									<option>NB-IoT</option>
+								</select>
+							</dd>
+							<dt>
+								<label htmlFor="mcc">Mobile Country Code:</label>
+							</dt>
+							<dd>
+								<select
+									id="mcc"
+									value={mcc}
+									onChange={({ target: { value } }) => {
+										setMcc(value)
+										u({
+											property: 'roam',
+											v: {
+												mccmnc: `${value}${mnc}`,
+											},
+										})
+									}}
+								>
+									{Object.entries(
+										MccMncList.all()
+											.filter(({ status }) => status === 'Operational')
+											.sort(({ countryName: c1 }, { countryName: c2 }) =>
+												(c1 ?? 'Z').localeCompare(c2 ?? 'Z'),
+											)
+											.reduce(
+												(mccs, { mcc, countryName }) => ({
+													...mccs,
+													[mcc]: countryName,
+												}),
+												{} as Record<string, string>,
+											),
+									).map(([mcc, countryName]) => (
+										<option key={mcc} value={mcc}>
+											{countryName ?? mcc} ({mcc})
+										</option>
+									))}
+								</select>
+							</dd>
+							<dt>
+								<label htmlFor="mnc">Mobile Network Code:</label>
+							</dt>
+							<dd>
+								{mcc === undefined && (
+									<select id="mnc" disabled>
+										<option>Please select a MCC</option>
+									</select>
+								)}
+								{mcc !== undefined && (
+									<select
+										id="mnc"
+										value={mnc}
+										onChange={({ target: { value } }) => {
+											setMnc(value)
+											u({
+												property: 'roam',
+												v: {
+													mccmnc: `${mcc}${value}`,
+												},
+											})
+										}}
+									>
+										{MccMncList.all()
+											.filter(({ mcc: m }) => m === mcc)
+											.sort(({ mnc: m1 }, { mnc: m2 }) => m1.localeCompare(m2))
+											.map(({ mnc, brand, operator }) => (
+												<option key={mnc} value={mnc}>
+													{brand} {operator} ({mnc})
+												</option>
+											))}
+									</select>
+								)}
+							</dd>
+							<dt>
+								<label htmlFor="cell">Cell:</label>
+							</dt>
+							<dd>
+								<input
+									id="cell"
+									type="number"
+									step={1}
+									min={0}
+									value={cell}
+									onChange={({ target: { value } }) => {
+										setCell(parseInt(value, 10))
+										u({
+											property: 'roam',
+											v: {
+												cell: value,
+											},
+										})
+									}}
+								/>
+							</dd>
+							<dt>
+								<label htmlFor="area">Area:</label>
+							</dt>
+							<dd>
+								<input
+									id="area"
+									type="number"
+									step={1}
+									min={0}
+									value={area}
+									onChange={({ target: { value } }) => {
+										setArea(parseInt(value, 10))
+										u({
+											property: 'roam',
+											v: {
+												area: value,
+											},
+										})
+									}}
+								/>
+							</dd>
 							<dt>RSRP: -{rsrp}</dt>
 							<dd>
 								<Slider
@@ -223,7 +364,13 @@ export const UpdateUI = ({
 						</DeviceInfoList>
 					</form>
 					<PGPS sendMessage={m} />
-					<AGPS sendMessage={m} />
+					<AGPS
+						sendMessage={m}
+						mnc={parseInt(mnc ?? '2', 10)}
+						mcc={parseInt(mcc ?? '242', 10)}
+						cell={cell ?? 33703719}
+						area={area ?? 12}
+					/>
 				</>
 			)}
 		</Device>
